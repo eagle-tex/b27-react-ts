733bec162ef152583fbe0974f157ec14763daefb
Gildas Montcho <gmontcho@gmail.com>
2023-10-29 00:29:59 +0200

    feat(App): import SignUpPage & render it in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,9 @@
-import { useState } from 'react';
+// import './App.css';
 
-import reactLogo from '@/assets/react.svg';
-
-import viteLogo from '../public/vite.svg';
-import './App.css';
+import SignUpPage from '@/pages/signup/SignUpPage.tsx';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={() => setCount((prev) => prev + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  return <SignUpPage />;
 }
 
 export default App;
