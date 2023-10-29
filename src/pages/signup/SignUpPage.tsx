a74bd949d59661e5390d43a580a8ad259409a7d3
Gildas Montcho <gmontcho@gmail.com>
2023-10-29 02:17:35 +0200

    feat(SignUpPage): pass #2-3 -> associate a label with each input

--- src/pages/signup/SignUpPage.tsx
+++ src/pages/signup/SignUpPage.tsx
@@ -1,12 +1,20 @@
-import { Input } from '@chakra-ui/react';
+import { FormControl, FormLabel, Input } from '@chakra-ui/react';
 // type Props = {}
 
 function SignUpPage(/* {}: Props */) {
   return (
     <div>
       <h1>Connexion</h1>
-      <Input placeholder="Identifiant" />
-      <Input placeholder="Email" />
+
+      <FormControl>
+        <FormLabel htmlFor="username">Identifiant</FormLabel>
+        <Input type="text" placeholder="Identifiant" id="username" />
+      </FormControl>
+
+      <FormControl>
+        <FormLabel htmlFor="email">Email</FormLabel>
+        <Input type="email" placeholder="Email" id="email" />
+      </FormControl>
     </div>
   );
 }
