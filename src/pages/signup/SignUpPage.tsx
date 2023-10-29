3e99af51a1e6936ea33789142848aae61aaa6969
Gildas Montcho <gmontcho@gmail.com>
2023-10-29 19:36:58 +0100

    feat: pass #8 and style the signup form

--- src/pages/signup/SignUpPage.tsx
+++ src/pages/signup/SignUpPage.tsx
@@ -1,35 +1,71 @@
-import { FormControl, FormLabel, Input } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+} from '@chakra-ui/react';
 // type Props = {}
 
 function SignUpPage(/* {}: Props */) {
   return (
-    <div>
-      <h1>Connexion</h1>
+    <Flex
+      bg="gray.50"
+      width="full"
+      height="100vh"
+      align="center"
+      justifyContent="center"
+    >
+      <Box
+        p={6}
+        minWidth={400}
+        maxWidth="500px"
+        borderWidth={1}
+        borderRadius={8}
+      >
+        <Box textAlign="center">
+          <Heading as="h1" fontWeight="thin" size="lg">
+            Connexion
+          </Heading>
+        </Box>
 
-      <FormControl>
-        <FormLabel htmlFor="username">Identifiant</FormLabel>
-        <Input type="text" placeholder="Identifiant" id="username" />
-      </FormControl>
+        <Box my={4} textAlign="left">
+          <form>
+            <FormControl>
+              <FormLabel htmlFor="username">Identifiant</FormLabel>
+              <Input type="text" placeholder="Identifiant" id="username" />
+            </FormControl>
 
-      <FormControl>
-        <FormLabel htmlFor="email">Email</FormLabel>
-        <Input type="email" placeholder="Email" id="email" />
-      </FormControl>
+            <FormControl mt={6}>
+              <FormLabel htmlFor="email">Email</FormLabel>
+              <Input type="email" placeholder="votre@email.com" id="email" />
+            </FormControl>
 
-      <FormControl>
-        <FormLabel htmlFor="password">Mot de passe</FormLabel>
-        <Input type="password" placeholder="Mot de passe" id="password" />
-      </FormControl>
+            <FormControl mt={6}>
+              <FormLabel htmlFor="password">Mot de passe</FormLabel>
+              <Input type="password" placeholder="********" id="password" />
+            </FormControl>
 
-      <FormControl>
-        <FormLabel htmlFor="passwordRepeat">Répéter mot de passe</FormLabel>
-        <Input
-          type="password"
-          placeholder="Répéter mot de passe"
-          id="passwordRepeat"
-        />
-      </FormControl>
-    </div>
+            <FormControl mt={6}>
+              <FormLabel htmlFor="passwordRepeat">
+                Confirmer mot de passe
+              </FormLabel>
+              <Input
+                type="password"
+                placeholder="********"
+                id="passwordRepeat"
+              />
+            </FormControl>
+
+            <Button colorScheme="teal" width="full" mt={6} mb={-3}>
+              Connexion
+            </Button>
+          </form>
+        </Box>
+      </Box>
+    </Flex>
   );
 }
 
