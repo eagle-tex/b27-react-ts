74a1e32188ef53f62582c464d3657e85270905e2
Gildas Montcho <gmontcho@gmail.com>
2023-10-30 11:44:52 +0100

    feat: pass #9 -> add `disabled` prop to the Button & a border around the form

--- src/pages/signup/SignUpPage.tsx
+++ src/pages/signup/SignUpPage.tsx
@@ -1,12 +1,3 @@
-// import {
-//   Box,
-//   Button,
-//   Flex,
-//   FormControl,
-//   FormLabel,
-//   Heading,
-//   Input,
-// } from '@chakra-ui/react';
 import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 
 // type Props = {}
@@ -23,8 +14,20 @@ function SignUpPage(/* {}: Props */) {
       px={4}
       my={6}
     >
-      {/* <Box textAlign="left"> */}
-      <Box p={6} minWidth="400px" maxWidth="500px">
+      {/* <Box texAlign="left"> */}
+      <Box
+        p={4}
+        minWidth="400px"
+        maxWidth="500px"
+        // borderWidth={1}
+        // borderRadius={8}
+        sx={{
+          border: 1,
+          borderColor: '#aaaccc',
+          borderWidth: 1,
+          borderRadius: 2,
+        }}
+      >
         <Typography
           component="h1"
           variant="h4"
@@ -75,68 +78,19 @@ function SignUpPage(/* {}: Props */) {
             fullWidth
           />
 
-          <Button color="success" size="large" variant="contained" fullWidth>
+          <Button
+            color="success"
+            size="large"
+            variant="contained"
+            fullWidth
+            disabled
+          >
             Connexion
           </Button>
           {/* </form> */}
         </Stack>
       </Box>
     </Box>
-    //   <Flex
-    //     bg="gray.50"
-    //     width="full"
-    //     height="100vh"
-    //     align="center"
-    //     justifyContent="center"
-    //   >
-    //     <Box
-    //       p={6}
-    //       minWidth={400}
-    //       maxWidth="500px"
-    //       borderWidth={1}
-    //       borderRadius={8}
-    //     >
-    //       <Box textAlign="center">
-    //         <Heading as="h1" fontWeight="thin" size="lg">
-    //           Connexion
-    //         </Heading>
-    //       </Box>
-    //
-    //       <Box my={4} textAlign="left">
-    //         <form>
-    //           <FormControl>
-    //             <FormLabel htmlFor="username">Identifiant</FormLabel>
-    //             <Input type="text" placeholder="Identifiant" id="username" />
-    //           </FormControl>
-    //
-    //           <FormControl mt={6}>
-    //             <FormLabel htmlFor="email">Email</FormLabel>
-    //             <Input type="email" placeholder="votre@email.com" id="email" />
-    //           </FormControl>
-    //
-    //           <FormControl mt={6}>
-    //             <FormLabel htmlFor="password">Mot de passe</FormLabel>
-    //             <Input type="password" placeholder="********" id="password" />
-    //           </FormControl>
-    //
-    //           <FormControl mt={6}>
-    //             <FormLabel htmlFor="passwordRepeat">
-    //               Confirmer mot de passe
-    //             </FormLabel>
-    //             <Input
-    //               type="password"
-    //               placeholder="********"
-    //               id="passwordRepeat"
-    //             />
-    //           </FormControl>
-    //
-    //           <Button colorScheme="teal" width="full" mt={6} mb={-3}>
-    //             Connexion
-    //           </Button>
-    //         </form>
-    //       </Box>
-    //     </Box>
-    //   </Flex>
   );
 }
 
