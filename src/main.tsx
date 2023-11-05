0734e547481ddc0a4cae927cef98d3b5400302fa
Gildas Montcho <gmontcho@gmail.com>
2023-11-05 10:38:14 +0100

    feat: add API mocking to development env (in the browser)

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,34 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App.tsx';
+// import { server } from './mocks/server.ts';
+//
+// if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
+//   server.listen();
+// }
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    {/* <ChakraProvider> */}
-    <App />
-    {/* </ChakraProvider> */}
-  </React.StrictMode>
-);
+async function deferRender() {
+  if (process.env.NODE_ENV !== 'development') {
+    return;
+  }
+
+  const { worker } = await import('./mocks/browser.ts');
+
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  await worker.start();
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+deferRender()
+  .then(() => {
+    root.render(
+      <React.StrictMode>
+        {/* <ChakraProvider> */}
+        <App />
+        {/* </ChakraProvider> */}
+      </React.StrictMode>
+    );
+  })
+  .catch((err) => console.log(err));
