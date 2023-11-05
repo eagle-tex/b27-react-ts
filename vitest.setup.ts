da754e52a2cb17c59e5915dc99f80df8ac3bcc29
Gildas Montcho <gmontcho@gmail.com>
2023-11-05 18:30:07 +0100

    chore(vitest.setup): remove console.log statement in beforeAll

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,8 +1,6 @@
 import { server } from './src/mocks/server.ts';
 
 beforeAll(() => {
-  console.log('beforeAll in VITEST.SETUP');
-
   server.events.on('request:start', ({ request }) => {
     console.log('MSW intercepted:', request.method, request.url);
   });
