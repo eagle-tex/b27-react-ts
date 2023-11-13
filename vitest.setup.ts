85ae99e0db613732ab3ec1ce17b5143fb206b35b
Gildas Montcho <gmontcho@gmail.com>
2023-11-13 21:50:09 +0100

    feat(vitest.setup): comment out logging on MSW request interception

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,9 +1,9 @@
 import { server } from './src/mocks/server.ts';
 
 beforeAll(() => {
-  server.events.on('request:start', ({ request }) => {
-    console.log('MSW intercepted:', request.method, request.url);
-  });
+  // server.events.on('request:start', (/* { request } */) => {
+  // console.log('MSW intercepted:', request.method, request.url);
+  // });
 
   return server.listen({ onUnhandledRequest: 'warn' });
 });
