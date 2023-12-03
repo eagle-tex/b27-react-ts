581f27a89e14dfd45acbb281daa474126abeffbc
Gildas Montcho <gmontcho@gmail.com>
2023-12-03 15:55:54 +0100

    feat(AccActivPage): pass #65 -> show Alert on activ. succ. w/ props & state - ERROR msg
    - create a piece of state: `result` and `setResult`
    
    - this implementation creates an error in the console -> to be resolved

--- src/pages/activation/AccountActivationPage.tsx
+++ src/pages/activation/AccountActivationPage.tsx
@@ -1,5 +1,8 @@
 import { Alert, Container } from '@mui/material';
+import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
 
+import { activate } from '@/api/apiCalls.ts';
 import i18n from '@/i18n/config.ts';
 
 type Props = {
@@ -12,15 +15,44 @@ type Props = {
 
 function AccountActivationPage({ match }: Props) {
   const { t } = i18n;
+  const [result, setResult] = useState('result');
+
+  useEffect(() => {
+    activate(match.params.token)
+      .then(() => {
+        setResult('success');
+      })
+      .catch(() => {
+        console.log({ where: 'activate - catch' });
+      });
+  }, []);
 
   return (
     <div data-testid="activation-page" style={{ paddingTop: '40px' }}>
-      <Container maxWidth="xs">
-        <Alert severity="success">{t('account.activated')}</Alert>
-        <p>{JSON.stringify(match)}</p>
-      </Container>
+      {result === 'success' && (
+        <Container maxWidth="xs">
+          <Alert severity="success">{t('account.activated')}</Alert>
+          <p>{JSON.stringify(match)}</p>
+        </Container>
+      )}
     </div>
   );
 }
+
+AccountActivationPage.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      token: PropTypes.string,
+    }),
+  }),
+};
+
+AccountActivationPage.defaultProps = {
+  match: {
+    params: {
+      token: 'dummy-token',
+    },
+  },
+};
 
 export default AccountActivationPage;
