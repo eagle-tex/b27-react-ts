937fabc0233cc6b6c76ecf5286ac1128d5894dfa
Gildas Montcho <gmontcho@gmail.com>
2023-12-03 12:21:01 +0100

    feat(AccActivPage): pass #64 -> create basic AccountActivationPage

--- src/pages/activation/AccountActivationPage.tsx
+++ src/pages/activation/AccountActivationPage.tsx
@@ -1,7 +1,24 @@
-function AccountActivationPage() {
+import { Alert, Container } from '@mui/material';
+
+import i18n from '@/i18n/config.ts';
+
+type Props = {
+  match: {
+    params: {
+      token: string;
+    };
+  };
+};
+
+function AccountActivationPage({ match }: Props) {
+  const { t } = i18n;
+
   return (
     <div data-testid="activation-page" style={{ paddingTop: '40px' }}>
-      <h1>Account Activation Page</h1>
+      <Container maxWidth="xs">
+        <Alert severity="success">{t('account.activated')}</Alert>
+        <p>{JSON.stringify(match)}</p>
+      </Container>
     </div>
   );
 }
