5b532136947521330063c357ad5bc210b306b6e3
Gildas Montcho <gmontcho@gmail.com>
2023-12-03 15:43:13 +0100

    test(AccActivPage.spec): add #65 'sends activ. req to backend' & setup mock server

--- src/pages/activation/AccountActivationPage.spec.tsx
+++ src/pages/activation/AccountActivationPage.spec.tsx
@@ -1,11 +1,33 @@
 import { render, screen } from '@testing-library/react';
+import { HttpResponse, http } from 'msw';
 
+import { BASE_URL } from '@/api/axiosConfig.ts';
 import fr from '@/locale/fr/translation.json';
+import { server } from '@/mocks/server.ts';
 
 import AccountActivationPage from './AccountActivationPage.tsx';
 
 const frTranslations = fr.account;
 const { activated: activatedFr } = frTranslations;
+
+let counter = 0;
+
+beforeEach(() => {
+  server.use(
+    http.post(`${BASE_URL}/api/v1/users/token/:token`, () => {
+      counter += 1;
+      return HttpResponse.json({ status: 200 });
+    })
+  );
+
+  counter = 0;
+});
+
+afterEach(() => {
+  server.resetHandlers();
+});
+
+afterAll(() => server.close());
 
 describe('Account Activation Page', () => {
   function setup(token: string) {
@@ -18,5 +40,12 @@ describe('Account Activation Page', () => {
     const message = await screen.findByText(activatedFr);
 
     expect(message).toBeInTheDocument();
+  });
+
+  it('065 - sends activation request to backend', async () => {
+    setup('1234');
+    await screen.findByText(activatedFr);
+
+    expect(counter).toBe(1);
   });
 });
